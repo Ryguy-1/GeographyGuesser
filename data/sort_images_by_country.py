8c1050b921f106e316cb71ca1f915f53cf652df0
Ryguy-1 <ryland.birchmeier.22@bishops.com>
2022-02-20 23:31:27 -0800

    switched to better reverse geocoding method

--- data/sort_images_by_country.py
+++ data/sort_images_by_country.py
@@ -1,15 +1,12 @@
 import os
 import glob
 from socketserver import ThreadingTCPServer
-from geopy.geocoders import Nominatim
+import reverse_geocoder
 import shutil
 
 # Globals
 raw_image_folder = "data/images"
 sorted_images_folder = "data/images_sorted_by_country"
-
-# initialize Nominatim API
-geolocator = Nominatim(user_agent="geoapiExercises")
 
 # Image Locations
 image_locations = glob.glob(raw_image_folder + "/*")
@@ -17,19 +14,21 @@ image_locations = glob.glob(raw_image_folder + "/*")
 # Iterate Through Images and Sort by Country
 transfer_counter = 0
 for file_loc in image_locations:
-    # Get Latitude and Longitude of png
-    lat = str(float(file_loc.split("\\")[-1].split(".p")[0].split("_")[0]))
-    long = str(float(file_loc.split("\\")[-1].split(".p")[0].split("_")[-1]))
-    location = geolocator.reverse(lat+","+long)
-    # Get Country
-    print(location.raw['address']['country'] + "|")
-    country = str(location.raw['address']['country']).lower().strip().replace(" ", "_").replace("/", "-")
-    if not os.path.exists(sorted_images_folder + "/" + country):
-        os.makedirs(sorted_images_folder + "/" + country)
-    # Make Copy of Image in New Folder
-    shutil.copy(file_loc, sorted_images_folder + "/" + country)
-    transfer_counter += 1
-    if transfer_counter % 100 == 0:
-        print("Transfered: " + str(transfer_counter) + " Images")
+    try:
+        # Get Latitude and Longitude of png
+        lat = float(file_loc.split("\\")[-1].split(".p")[0].split("_")[0])
+        long = float(file_loc.split("\\")[-1].split(".p")[0].split("_")[-1])
+        # Get Country of Image
+        location = reverse_geocoder.search((lat, long), mode=1)
+        country = location[0]['cc']
+        if not os.path.exists(sorted_images_folder + "/" + country):
+            os.makedirs(sorted_images_folder + "/" + country)
+        # Make Copy of Image in New Folder
+        shutil.copy(file_loc, sorted_images_folder + "/" + country)
+        transfer_counter += 1
+        if transfer_counter % 100 == 0:
+            print("Transfered: " + str(transfer_counter) + " Images")
+    except:
+        print("Error: " + file_loc)
 
 
